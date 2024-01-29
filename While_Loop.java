c58f843ef4508e8ea102be3ed22db17530cc746e
ParitoshBitode <paritosh.bitode@gmail.com>
2024-01-30 00:34:08 +0530

    While Loop in java

---
+++ While_Loop.java
@@ -0,0 +1,17 @@
+package loops;
+
+public class While_Loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i= 5;
+		while(i<10)
+		{
+			i++;
+			System.out.println("Hello");
+		}
+		
+	}
+
+}
